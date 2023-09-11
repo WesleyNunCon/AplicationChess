b1e598567534366bf657ad1c7b29687cdb3c97a2
WesleyNunCon <wesleynuncon@gmail.com>
2023-09-11 19:20:31 -0300

    first class

---
+++ src/aplicationChess/Aplication.java
@@ -0,0 +1,16 @@
+package aplicationChess;
+
+import boardgame.Position;
+
+public class Aplication {
+
+	public static void main(String[] args) {
+		
+		
+         Position pos = new Position(3, 5);
+		System.out.println(pos);
+
+		
+	}
+
+}
