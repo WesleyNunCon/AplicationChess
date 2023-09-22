2d4a6b7947861774fbb4bd582b7cf8c5d5fbab5b
WesleyNunCon <wesleynuncon@gmail.com>
2023-09-22 19:32:26 -0300

    array para captura de peças

--- src/aplicationChess/Aplication.java
+++ src/aplicationChess/Aplication.java
@@ -1,6 +1,8 @@
 package aplicationChess;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -14,12 +16,13 @@ public class Aplication {
 
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-
+        List<ChessPiece> captured = new ArrayList<>();
+		
 		while (true) {
 			try {
 				UI.clearScreen();
 	
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source:");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -34,6 +37,9 @@ public class Aplication {
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
 			
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
